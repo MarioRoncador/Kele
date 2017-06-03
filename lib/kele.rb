c5cf4dbe5452473cffbcac3ce18c5d8ee32c7cde
MarioRoncador <mroncador@alaska.edu>
2017-06-03 15:36:15 -0400

    Unauthorized message for checkpoint submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -30,6 +30,22 @@ class Kele
     @mentor_availability = JSON.parse(response.body)
   end
 
+  #checkpoint_id = 1905
+  #assignment_branch = https://github.com/MarioRoncador/Kele/tree/keleCP6
+  #assignment_commit_link = https://github.com/MarioRoncador/Kele/commit/f8629d78cad1f890c6b5f7dbf6120e8916fe16ae
+  #my enrollment_id = 2352229
+  def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    options = {body: {"checkpoint_id" => checkpoint_id, "assignment_branch" => assignment_branch, "assignment_commit_link" => assignment_commit_link, "comment" => comment}.merge(user_auth)}
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", options)
+    if response.success?
+      "Your checkpoint #{checkpoint_id} was submitted successfully"
+      response
+    else
+      "Error"
+      response
+    end
+  end
+
   #method used in the above 'get_me' and 'get_mentor_availability' to keep them cleaner
   def user_auth
     { headers: { "authorization" => @auth_token }}
